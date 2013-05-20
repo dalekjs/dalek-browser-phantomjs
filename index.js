e3296a53a41c70a46043650541f0ed7c58c04491
asciidisco <public@asciidisco.com>
2013-05-20 20:32:02 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+var Q = require('q');
+var spawn = require('child_process').spawn;
+var phantomjs = require('phantomjs');
+
+module.exports = {
+	name: 'PhantomJS',
+	version: phantomjs.version,
+	port: 9001,
+	host: 'localhost',
+	path: '/wd/hub',
+	spawned: null,
+	launch: function () {
+		var deferred = Q.defer();
+		var stream = '';
+	    this.spawned = spawn(phantomjs.path, ['--webdriver', '9001']);
+
+	    this.spawned.stdout.on('data', function (data) {
+	        var dataStr = new String(data);
+	        stream += dataStr;
+	        if (stream.search('GhostDriver - Main - running') !== -1) {
+	        	deferred.resolve();
+	        }
+	    });
+	    return deferred.promise;
+	},
+	kill: function () {
+		this.spawned.kill('SIGHUP');
+	}
+};
